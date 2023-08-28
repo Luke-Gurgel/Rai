bc178571beb9a795532f9e0bacafecc9394d1658
Lucas Gurgel <lucas.gurgeldeandrade@childrens.harvard.edu>
2023-08-27 21:41:25 -0300

    adjust header and footer padding

--- src/components/PageFooter/index.tsx
+++ src/components/PageFooter/index.tsx
@@ -2,7 +2,7 @@ import { Sheet, Typography } from "@mui/joy";
 
 export const PageFooter = () => {
   return (
-    <div className="w-full px-16 py-8 bg-slate-200">
+    <div className="w-full px-16 py-6 bg-slate-200">
       <Sheet className="bg-transparent flex align-center">
         <Typography level="body-lg">Feito com 💙 por Lucas</Typography>
       </Sheet>
